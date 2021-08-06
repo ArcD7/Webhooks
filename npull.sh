a1d38d875529ed5c7480e2aa6c97b2597b9c9b70
Archit <pokemongochamp47@gmail.com>
2021-08-07 01:15:35 +0530

    New mechanism for the working of Git

---
+++ npull.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+cd /home/archit/Webhooks
+echo "$BRANCH"
+
+git_branch=$(git branch | grep "*" | sed 's/*//' | sed 's/^ *//g')
+echo "$git_branch"
+
+git_status=$(git status | grep "Changes to be committed")
+echo "$git_status"
+
+#check the current branch is same as git branch output
+if [ $git_branch=="local" ]; then
+	if [ "$git_status"=="Changes to be committed" ]; then
+		git stash
+		git pull
+	else
+		git pull
+	fi
+else
+	if [ $git_status== "Changes to be committed" ]; then
+		echo "Perform git stash"
+		echo "Perform git checkout to the specific branch"
+		echo "Perform git pull"
+	else
+		echo "Perform git checkout to the specific branch"
+		echo "Perform git pull"
+	fi
+
+fi
+
